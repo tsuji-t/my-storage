27e2978e0f0d62ed43684d1867a35c4d6c22ad50
tsuji-t <tsuji6911tomoki@gmail.com>
2021-03-08 17:18:31 +0900

    テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_100751) do
+ActiveRecord::Schema.define(version: 2021_03_08_075325) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +25,15 @@ ActiveRecord::Schema.define(version: 2021_03_05_100751) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "words", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.string "tug"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_words_on_user_id"
+  end
+
+  add_foreign_key "words", "users"
 end
