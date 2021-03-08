aa5532f83d53a702c848da0f3169e6fed256c571
tsuji-t <tsuji6911tomoki@gmail.com>
2021-03-08 18:35:02 +0900

    投稿画面作成

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,4 +2,20 @@ class WordsController < ApplicationController
 
   def index
   end
+
+  def new
+    @word = Word.new
+  end
+
+  def create
+    @word = Word.new
+    if @word.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
 end
