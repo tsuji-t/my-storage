576278f64bc051a6146cfa5cfb8dc1cb05b6d154
tsuji-t <tsuji6911tomoki@gmail.com>
2021-03-11 15:17:32 +0900

    コメントフォーム作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+
+  def create
+   @comment = Comment.new
+   @comment.save(comment_params)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(message).merge(user_id: currend_user.id, word_id: params[:word_id])
 end
