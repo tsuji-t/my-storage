44ffb2fad085b14e18c1ff2a37f1305b6b0a7385
tsuji-t <tsuji6911tomoki@gmail.com>
2021-03-11 12:42:41 +0900

    ルーザー投稿一覧機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
+  before_action :side_ber
 
   def show
+    @user = User.find(params[:id])
+    @words = @user.words
   end
-  
+
+  private
+
+  def side_ber
+    @side_words = Word.includes(:user).order('created_at DESC')
+  end
+
 end
