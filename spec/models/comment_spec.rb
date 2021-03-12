81020f7dc8a9d741c38060a3c0278137a10c2d4a
tsuji-t <tsuji6911tomoki@gmail.com>
2021-03-12 18:14:49 +0900

    単体テスト実装

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
+# 実行コマンド
+# bundle exec rspec spec/models/comment_spec.rb
+
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント投稿' do
+    it 'コメント投稿ができる' do
+      expect(@comment).to be_valid
+    end
+
+    it 'コメントが空だと投稿できない' do
+      @comment.message = ""
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("コメントを入力してください")
+    end
+
+    it 'ユーザーが空だと投稿できない'do
+      @comment.user = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("ユーザー名を入力してください")
+    end
+
+    it '記事が空だと投稿できない'do
+      @comment.word = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("記事を入力してください")
+    end
+  end
 end
