8f36160fa9418423afe83c36bdb69abf7da09569
tsuji-t <tsuji6911tomoki@gmail.com>
2021-03-18 19:28:28 +0900

    修正

--- app/javascript/editor.js
+++ app/javascript/editor.js
@@ -3,5 +3,11 @@ import SimpleMDE from "simplemde";
 window.onload = function () {
   const simplemde = new SimpleMDE({
     element: document.getElementById("editor"),
+    spellChecker: false,
+    toolbar: ["bold", "horizontal-rule", "heading","|",
+              "quote", "unordered-list", "ordered-list","|",
+              "code", "link", "table", "|",
+              "preview", "side-by-side", "fullscreen", "|",
+              "guide"]
   });
 };
