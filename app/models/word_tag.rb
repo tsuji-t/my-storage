fea017469b796b91e45ab85d487365d0d330744d
tsuji-t <tsuji6911tomoki@gmail.com>
2021-04-25 17:22:02 +0900

    tagテーブル作成

---
+++ app/models/word_tag.rb
@@ -0,0 +1,2 @@
+class WordTag < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_103034) do
+ActiveRecord::Schema.define(version: 2021_04_25_081503) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "message", null: false
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2021_03_19_103034) do
     t.index ["word_id"], name: "index_favorites_on_word_id"
   end
 
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,6 +48,15 @@ ActiveRecord::Schema.define(version: 2021_03_19_103034) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "word_tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "word_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_id"], name: "index_word_tags_on_tag_id"
+    t.index ["word_id"], name: "index_word_tags_on_word_id"
   end
 
   create_table "words", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -58,5 +73,7 @@ ActiveRecord::Schema.define(version: 2021_03_19_103034) do
   add_foreign_key "comments", "words"
   add_foreign_key "favorites", "users"
   add_foreign_key "favorites", "words"
+  add_foreign_key "word_tags", "tags"
+  add_foreign_key "word_tags", "words"
   add_foreign_key "words", "users"
 end
