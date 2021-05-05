1af6d3d865904f8f143c2c3cd992b509f6335ba0
tsuji-t <tsuji6911tomoki@gmail.com>
2021-05-05 14:28:18 +0900

    formオブジェクト作成

---
+++ app/models/form_words_tag.rb
@@ -0,0 +1,19 @@
+class FormwordsTag
+
+  include ActiveModel::Model
+  attr_accessor :title, :text, :tug, :name, :user_id,
+
+  with_options presence: true do
+    validates :title
+    validates :text
+    validates :name
+  end
+
+  def save
+    word = Tweet.create(title: title, text: text, tug: tug, user_id: current_user.id)
+    tag = Tag.create(name: name)
+
+    WordTag.create(word_id: word.id, tag_id: tag.id)
+  end
+
+end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,6 @@ class Tag < ApplicationRecord
   has_many :word_tags
   has_many :words, through: :word_tags
 
+  validates :name, uniqueness: true
+
 end
